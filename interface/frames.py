7b4a6d88173e45413446cee89d03561b37550232
kaiosatiro <kaiosatiro@gmail.com>
2023-08-18 22:47:02 -0300

    Interface - tela pesquisa

---
+++ interface/frames.py
@@ -0,0 +1,191 @@
+import customtkinter as ctk
+from PIL import Image
+import os
+from CTkTable import *
+
+
+class FramePet(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_rowconfigure(4, weight=1)
+
+        self.var_nome = ctk.StringVar(name='PY_NOME_PET', value='-')
+        self.var_raca = ctk.StringVar(name='PY_RACA', value='-')
+        self.var_porte = ctk.StringVar(name='PY_PORTE', value='-')
+        self.var_observacoes = ctk.StringVar(name='PY_OBERVACOES', value='...')
+        
+
+        self.img_frame =  ctk.CTkFrame(self, fg_color='transparent')
+        self.img_frame.grid(row=0, column=0, rowspan=3, sticky='nsew', padx=10, pady=10)
+        # image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
+        self.foto = ctk.CTkImage(Image.open(os.path.realpath("images/image.png")), size=(500,300))#
+        self.foto_label = ctk.CTkLabel(self.img_frame, text="", image=self.foto)
+        self.foto_label.grid(row=0, column=0, sticky='nsew')
+
+        self.nome = ctk.CTkEntry(
+            self, textvariable=self.var_nome,
+            fg_color='transparent', state='readonly',
+            font=('', 32, 'normal'), justify='center'
+        ).grid(row=0, column=2, padx=10, pady=10, sticky='nsew')
+        self.label_nome = ctk.CTkLabel(self, text='Nome:', font=('', 32, 'bold')).grid(row=0, column=1)
+        
+        self.raca = ctk.CTkEntry(
+            self, textvariable=self.var_raca,
+            fg_color='transparent', state='readonly',
+            font=('', 32, 'normal'), justify='center'
+        ).grid(row=1, column=2, padx=10, pady=10, sticky='nsew')
+        self.label_raca = ctk.CTkLabel(self, text='Raça:', font=('', 32, 'bold')).grid(row=1, column=1)
+
+        self.porte = ctk.CTkEntry(
+            self, textvariable=self.var_porte,
+            fg_color='transparent', state='readonly',
+            font=('', 32, 'normal'), justify='center'
+        ).grid(row=2, column=2, padx=10, pady=10, sticky='nsew')
+        self.label_porte = ctk.CTkLabel(self, text='Porte:', font=('', 32, 'bold')).grid(row=2, column=1)
+
+
+        self.label_observacoes = ctk.CTkLabel(self, text='Observações:', font=('', 26, 'bold'))
+        self.label_observacoes.grid(row=3, column=0, sticky='w', padx=10, pady=5)
+        self.observacoes = ctk.CTkTextbox(
+            self, corner_radius=6, font=('', 22, 'normal'),
+            fg_color='transparent'
+            )
+        self.observacoes.grid(row=4, column=0, columnspan=3, sticky='nsew', padx=10, pady=(0, 10))
+        self.observacoes.insert("0.0", self.var_observacoes.get())
+
+
+    def get(self):
+        return {
+            'nome': self.var_nome.get(),
+            'raca': self.var_raca.get(),
+            'porte': self.var_porte.get(),
+            'obervacoes': self.var_observacoes.get()
+        }
+    
+
+    def set(self, nome:str, raca:str, porte:str, observacoes:str):
+        self.var_nome.set(nome)
+        self.var_raca.set(raca)
+        self.var_porte.set(porte)
+        self.var_observacoes.set(observacoes)
+
+        self.observacoes.delete("0.0", "end") 
+        self.observacoes.insert("0.0", self.var_observacoes.get())
+        
+    
+class FrameTutor(ctk.CTkFrame):
+    def __init__(self, master, titulo):
+        super().__init__(master)
+        
+        self.grid_columnconfigure((1,3), weight=1)
+        self.grid_rowconfigure(4, weight=1)
+
+        self.var_nome = ctk.StringVar(name='PY_NOME_TUTOR', value='-')
+        self.var_tel1 = ctk.StringVar(name='PY_TEL1_TUTOR', value='-')
+        self.var_tel2 = ctk.StringVar(name='PY_TEL2_TUTOR', value='-')
+        self.var_endereco = ctk.StringVar(name='PY_ENDERECO', value='...')
+
+        self.titulo = ctk.CTkLabel(
+            self, text=titulo, font=('', 28, 'bold'),
+        ).grid(row=0, column=0, padx=10, pady=5, columnspan=4)
+        
+
+        self.label_nome = ctk.CTkLabel(
+            self, text='Nome:', font=('', 22, 'bold'),          
+        ).grid(row=1, column=0, padx=10, pady=5, sticky='w')
+
+        self.nome = ctk.CTkEntry(
+            self, textvariable=self.var_nome,
+            fg_color='transparent', state='readonly',
+            font=('', 22, 'normal')
+        ).grid(row=1, column=1, columnspan=3, pady=5, sticky='we')
+        
+
+        self.label_tel1 = ctk.CTkLabel(self, text='Tel1:', font=('', 22, 'bold')).grid(
+            row=2, column=0, sticky='w', padx=10, pady=5
+        )
+
+        self.tel1 = ctk.CTkEntry(
+            self, textvariable=self.var_tel1,
+            fg_color='transparent', state='readonly',
+            font=('', 22, 'normal')
+        ).grid(row=2, column=1, sticky='we')
+
+
+        self.label_tel2 = ctk.CTkLabel(self, text='Tel2:', font=('', 22, 'bold')).grid(
+            row=2, column=2, sticky='w', padx=10, pady=5
+        )
+
+        self.tel1 = ctk.CTkEntry(
+            self, textvariable=self.var_tel2,
+            fg_color='transparent', state='readonly',
+            font=('', 22, 'normal'), justify='center'
+        ).grid(row=2, column=3, sticky='we')
+
+
+        self.label_endereco = ctk.CTkLabel(self, text='Endereço:', font=('', 22, 'bold')).grid(
+            row=3, column=0, padx=10, pady=5, sticky='w', columnspan=2
+        )
+    
+        self.endereco = ctk.CTkTextbox(
+            self, corner_radius=6, font=('', 22, 'normal'),
+            fg_color='transparent'
+            )
+        self.endereco.grid(row=4, column=0, columnspan=4, sticky='nsew')
+        self.endereco.insert("0.0", self.var_endereco.get())  
+    
+
+    def get(self):
+        return {
+            'nome':self.var_nome.get(),
+            'tel1': self.var_tel1.get(),
+            'tel2': self.var_tel2.get(),
+            'endereco':self.var_endereco.get()
+        }
+    
+
+    def set(self, nome:str, tel1:str, tel2:str, endereco:str):
+        self.var_nome.set(nome)
+        self.var_tel1.set(tel1)       
+        self.var_tel2.set(tel2)       
+        self.var_endereco.set(endereco)
+
+        self.endereco.delete("0.0", "end") 
+        self.endereco.insert("0.0", self.var_endereco.get())  
+
+
+class FramePesquisa(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.head_pet = [['#', 'Nome', 'Raça', 'Tutor 1', 'Tutor 2'], ['1', 'Marley', 'Labrador', 'Caio Satiro']]
+        self.head_tutor = [['#', 'Nome', 'Telefone 1', 'Telefone 2'], ['1', 'Caio Satiro', '11981855170']]
+
+        self.tabela = CTkTable(
+            self, column=5, values=self.head_pet,
+            hover=True, hover_color='grey', header_color='grey',
+            font=('', 22, 'normal')
+        )
+
+        self.tabela.columnconfigure(0, weight=0)
+        self.tabela.columnconfigure(1, weight=2)
+        self.tabela.columnconfigure((2,3,4), weight=1)
+        # self.tabela.rowconfigure(0, weight=1)
+
+        self.tabela.pack(expand=True, fill="both", padx=20, pady=20)
+
+    def muda_header(self, tipo):
+        if tipo == 'PET':
+            self.tabela.configure(values=self.head_pet)
+        elif tipo == 'TUTOR':
+            self.tabela.configure(values=self.head_tutor)
+
+
+# class FrameServicos(ctk.CTkFrame):
+#     def __init__(self, master):
+#         super().__init__(master)
+
+#         head = [['#', 'Serviço', 'Data']]
+#         self.tabela = CTkTable(self, column=3, values=head)
+#         self.tabela.pack(expand=True, fill="both", padx=20, pady=20)
---
+++ interface/main.py
@@ -0,0 +1,103 @@
+from frames import *
+
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        
+        self.title('petApp')
+        self.geometry('1280x720')
+        # self.attributes('-fullscreen', True)        
+        # self.globalgetvar('PY_NOME_PET')
+
+        #AREA PRINCIPAL
+        self.pet = FramePet(self)
+        self.tutor1 = FrameTutor(self, 'Tutor 1')   
+        self.tutor2 = FrameTutor(self, 'Tutor 2')
+
+        self.BT_pesquisar = ctk.CTkButton(
+            self, text='Pesquisar',
+            font=('', 32, 'normal'),
+            border_spacing=8,
+            command=self.pesquisa_button_event
+        )
+
+        #AREA DE PESQUISA
+        self.var_busca = ctk.StringVar(name='PY_BUSCA', value='')
+        self.var_tipo_busca = ctk.StringVar(name='PY_TIPO_BUSCA')
+        
+        self.label_busca = ctk.CTkLabel(self, text='Busca:', font=('', 32, 'normal'))
+        self.busca = ctk.CTkEntry(
+            self, textvariable=self.var_busca,
+            font=('', 22, 'normal')
+        )
+        
+        self.radio_pet = ctk.CTkRadioButton(self, text='PET', font=('', 26, 'normal'), value='PET', variable=self.var_tipo_busca, command=self._radio_callback)
+        self.radio_tutor = ctk.CTkRadioButton(self, text='TUTOR', font=('', 26, 'normal'), value='TUTOR', variable=self.var_tipo_busca, command=self._radio_callback)
+        self.var_tipo_busca.set('PET')
+
+        self.tabela_resultado = FramePesquisa(self)
+        self._radio_callback()
+        # self.servicos = FrameServicos(self).grid(row=3, column=0, padx=10, pady=(10, 10), sticky='ewsn', columnspan=2)
+
+        self.seleciona_frame("pesquisa")
+    
+    def _radio_callback(self):
+        self.tabela_resultado.muda_header(self.var_tipo_busca.get())
+
+    def seleciona_frame(self, frame):
+        if frame == "home":
+            self.grid_columnconfigure(0, weight=2)
+            self.grid_columnconfigure(1, weight=1)
+            self.grid_rowconfigure((0,3), weight=0)
+            self.grid_rowconfigure((1,2), weight=1)
+            self.pet.grid(row=1, column=0, padx=10, pady=(10, 0), rowspan=2, sticky='ewsn')
+            self.tutor1.grid(row=1, column=1, padx=10, pady=(10, 0), sticky='ewsn')
+            self.tutor2.grid(row=2, column=1, padx=10, pady=(10, 0), sticky='ewsn')
+            self.BT_pesquisar.grid(row=3, column=0, sticky='nsew', padx=10, pady=10)
+        else:
+            self.pet.grid_forget()
+            self.tutor1.grid_forget()
+            self.tutor2.grid_forget()
+            self.BT_pesquisar.grid_forget()
+
+        if frame == "pesquisa":
+            self.grid_columnconfigure(0, weight=0)
+            self.grid_columnconfigure(1, weight=1)
+            self.grid_rowconfigure(1, weight=0)
+            self.grid_rowconfigure(2, weight=1)
+            self.label_busca.grid(row=0, column=0, padx=(30, 10), pady=(30, 10))
+            self.busca.grid(row=0, column=1, sticky='new', padx=30, pady=(30, 10))
+            self.radio_pet.grid(row=1, column=0, sticky='n', padx=(30, 10), pady=10)
+            self.radio_tutor.grid(row=1, column=1, sticky='nw', padx=(0, 10), pady=10)
+            self.tabela_resultado.grid(row=2, column=0, columnspan=2, sticky='new', padx=10, pady=10)
+            
+        else:
+            self.tabela_resultado.grid_forget()
+            self.busca.grid_forget()
+            self.label_busca.grid_forget()
+            self.radio_pet.grid_forget()
+            self.radio_tutor.grid_forget()
+
+        if frame == "cadastro":
+            ...
+        else:
+            ...
+        
+
+    def home_button_event(self):
+        self.seleciona_frame("home")
+
+    def pesquisa_button_event(self):
+        self.seleciona_frame("pesquisa")
+
+    def cadatro_button_event(self):
+        self.seleciona_frame("cadastro")
+
+
+
+
+if __name__ == '__main__':
+    ctk.set_appearance_mode("light")
+    app = App()
+    app.mainloop()  
